c8a2f60281d61797700f7a225068d217e728a4a3
DavidAnderson <37078097+daveAnderson514@users.noreply.github.com>
2025-02-16 21:16:50 -0600

    Print the grid

--- AStar.cpp
+++ AStar.cpp
@@ -2,9 +2,10 @@
 #include <vector>
 using namespace std;
 
+void PrintGrid(std::vector<std::vector<int>> grid); // Function declaration
+
 int main()
 {
-    std::cout << "Hello World" << std::endl;
     std::vector<std::vector<int>> grid = 
     {{0, 1, 0, 0, 0, 0},
     {0, 1, 0, 0, 0, 0},
@@ -12,5 +13,19 @@ int main()
     {0, 1, 0, 0, 0, 0},
     {0, 0, 0, 0, 1, 0}};
 
+    PrintGrid(grid);
+
     return 0;
+}
+
+void PrintGrid(std::vector<std::vector<int>> grid)
+{
+    for (int i = 0; i < grid.size(); i++)
+    {
+        for (int j = 0; j < grid[i].size(); j++)
+        {
+            std::cout << grid[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
 }
