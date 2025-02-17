163bf4d9d4601ac323719f3334620ae1562d40ac
DavidAnderson <37078097+daveAnderson514@users.noreply.github.com>
2025-02-16 21:01:47 -0600

    Make a grid

---
+++ AStar.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World" << std::endl;
+    std::vector<std::vector<int>> grid = 
+    {{0, 1, 0, 0, 0, 0},
+    {0, 1, 0, 0, 0, 0},
+    {0, 1, 0, 0, 0, 0},
+    {0, 1, 0, 0, 0, 0},
+    {0, 0, 0, 0, 1, 0}};
+
+    return 0;
+}
