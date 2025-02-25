1d8fc66b93cfcc2a7518ed46ab421be78bd1b8f4
DavidAnderson <37078097+daveAnderson514@users.noreply.github.com>
2025-02-24 21:53:43 -0600

    Read from file and store data

--- AStar.cpp
+++ AStar.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <string>
+#include <sstream>
 using namespace std;
+using std::istringstream;
 
 void PrintGrid(std::vector<std::vector<int>> grid); // Function declaration
+std::vector<std::vector<int>> ReadGridFromFile();
 
 int main()
 {
-    std::vector<std::vector<int>> grid = 
-    {{0, 1, 0, 0, 0, 0},
-    {0, 1, 0, 0, 0, 0},
-    {0, 1, 0, 0, 0, 0},
-    {0, 1, 0, 0, 0, 0},
-    {0, 0, 0, 0, 1, 0}};
-
+    std::vector<std::vector<int>> grid = ReadGridFromFile();
+    
     PrintGrid(grid);
 
     return 0;
@@ -28,4 +28,30 @@ void PrintGrid(std::vector<std::vector<int>> grid)
         }
         std::cout << std::endl;
     }
+}
+
+std::vector<std::vector<int>> ReadGridFromFile()
+{
+    std::vector<std::vector<int>> grid;
+    std::ifstream myFile;
+    myFile.open("1.board");
+    if (myFile.is_open())
+    {
+        std::string line;
+        
+        while (getline(myFile, line))
+        {
+            istringstream my_stream(line);
+            std::vector<int> row;
+            int n;
+            char c;
+
+            while(my_stream >> n >> c)
+            {
+                row.push_back(n);
+            }
+            grid.push_back(row);
+        }
+    }
+    return grid;
 }
